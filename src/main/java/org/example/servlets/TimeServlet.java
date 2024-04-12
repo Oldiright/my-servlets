820ceb9948d6d513f933edf9023bc13d496c2ab3
oldiright <oliynuk_Dima@ukr.net>
2024-04-12 23:10:42 +0300

    Main.java relocated

--- src/main/java/org/example/servlets/TimeServlet.java
+++ src/main/java/org/example/servlets/TimeServlet.java
@@ -8,24 +8,33 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.Map;
 
 @WebServlet(value = "/time")
 public class TimeServlet extends HttpServlet {
+    public static final String TIME_ZONE_PATH = "timezone";
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         LocalDateTime localDateTime = LocalDateTime.now();
         String[] rowLocalDateTime = localDateTime.toString().split("T");
         StringBuilder stringBuilder = new StringBuilder();
-        String timezone = req.getParameter("timezone");
         stringBuilder.append(rowLocalDateTime[0]).append(" ").append(rowLocalDateTime[1].split("\\.")[0]);
-        if(timezone != null) {
-            stringBuilder.append(" ").append(timezone);
+        if (hasParameter(req, TIME_ZONE_PATH)) {
+            stringBuilder.append(" ").append(req.getParameter(TIME_ZONE_PATH));
         } else {
             stringBuilder.append(" UTC");
         }
         resp.setContentType("text/html: charset=utf-8");
         resp.getWriter().write(stringBuilder.toString());
         resp.getWriter().close();
+    }
+
+    private boolean hasParameter(HttpServletRequest req, String parameterName) {
+        Map<String, String[]> parameterMap = req.getParameterMap();
+        if(!parameterMap.containsKey(parameterName) || parameterMap.get(parameterName) == null) {
+            return false;
+        }
+        return true;
     }
 }
--- src/main/java/org/example/servlets/TimezoneValidateFilter.java
+++ src/main/java/org/example/servlets/TimezoneValidateFilter.java
@@ -3,6 +3,6 @@ package org.example.servlets;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpFilter;
 @WebFilter(value = "/time")
-public class TimezoneValidateFilter extends HttpFilter {
+public class  TimezoneValidateFilter extends HttpFilter {
 
 }
