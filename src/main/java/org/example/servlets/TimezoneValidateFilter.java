094ae68e1d6bec55a2308160509eeddda09fb755
oldiright <oliynuk_Dima@ukr.net>
2024-04-12 19:38:04 +0300

    Created new WebFilter TimezoneValidateFilter.java

---
+++ src/main/java/org/example/servlets/TimezoneValidateFilter.java
@@ -0,0 +1,8 @@
+package org.example.servlets;
+
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+@WebFilter(value = "/time")
+public class TimezoneValidateFilter extends HttpFilter {
+
+}
